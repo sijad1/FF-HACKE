43324b0cdcf3f06cb8fdd9a7391de5476e40d09d
sAhA123riAr <95167395+sijad1@users.noreply.github.com>
2022-02-11 22:57:47 +0600

    Add files via upload

---
+++ S-Bomb_BD.py
@@ -0,0 +1,21 @@
+import os,sys,time,datetime
+blue="\33[94m"
+red="\033[91m"
+lightblue="\033[94m"
+white="\33[97m"
+yellow="\33[93m"
+green="\033[1;32m"
+cyan="\033[96m"
+end="\033[0m"
+
+print(yellow+"hello")
+
+os.system('clear')
+info=""" ===========================================
+           [•]Created by sahariar hosen
+           [•] Help to creat THBD Admin(mao&Noob)
+           [•]Thank you THBD group Admin
+  ==================================================
+"""
+os.system("lolcat 002.txt")
+print(green+info)
